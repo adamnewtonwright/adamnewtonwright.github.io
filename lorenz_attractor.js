803af73f5be70784147629b59331cf9b62578da1
Adam Wright <adam@Adams-MacBook-Pro-4.local>
2017-06-20 17:33:14 -0400

    trying to figure shit out

--- lorenz_attractor.js
+++ lorenz_attractor.js
@@ -18,13 +18,13 @@ function plot_attractor()
 		var n = 100;
 		/* User can change value of constants */
 		var inputs = document.getElementById("frm1");
-		var a = eval(inputs.elements[1].value);
-		var b = eval(inputs.elements[2].value);
-		var c = eval(inputs.elements[3].value);
-		var n = eval(inputs.elements[4].value);
+		a = eval(inputs.elements[1].value);
+		b = eval(inputs.elements[2].value);
+		c = eval(inputs.elements[3].value);
+		n = eval(inputs.elements[4].value);
 		var text = "";
 		text = a + "<br>" + b + "<br>" + c + "<br>";
-		document.getElementById("demo").innerHTML = n;
+		//document.getElementById("demo").innerHTML = n;
 		var x = [], y = [], z = [];
 		/* Step size */
 		var dt = 0.015;
